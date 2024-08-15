e681582ef7207d5ed17129aa193c3830c2f235dc
Makihiro <mackysoft0129@gmail.com>
2024-08-15 15:14:06 +0900

    Add ItemMasterData

---
+++ Unity/Assets/Example/Scripts/Runtime/MasterData/Definitions/ItemMasterData.cs
@@ -0,0 +1,25 @@
+﻿using MasterMemory;
+using MessagePack;
+
+namespace MackySoft.MasterTools.Example
+{
+	[ImportTableFrom("Item")]
+	[MemoryTable("Item")]
+	[MessagePackObject]
+	public sealed class ItemMasterData
+    {
+
+        [PrimaryKey]
+		[Key(0)]
+		public int Id { get; private set; }
+
+		[Key(1)]
+		public string Name { get; private set; }
+
+        public ItemMasterData (int Id, string Name)
+        {
+            this.Id = Id;
+            this.Name = Name;
+        }
+    }
+}
